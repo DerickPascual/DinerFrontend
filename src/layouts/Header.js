897d60234f7bdd7c63dedb1caf98a1bfeb457f4e
Derick Pascual <derickvanpascual@gmail.com>
2023-07-06 23:57:58 -0700

    Quickly added header link

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,9 +1,16 @@
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    const handleDinderClick = () => {
+        navigate('/home');
+    }
+
     return (
         <div>
-            <h1 className='dinder'>Dinder</h1>
+            <h1 className='dinder'><a onClick={handleDinderClick}>Dinder</a></h1>
         </div>
     )
 };
