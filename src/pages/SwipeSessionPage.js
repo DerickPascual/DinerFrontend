052408a85eb9ddd8309c817bd2888cf23210d6f3
Derick Pascual <derickvanpascual@gmail.com>
2023-07-11 01:24:56 -0700

    Removed mui because dependency bug. Using react-simple-star-rating instead.

--- src/pages/SwipeSessionPage.js
+++ src/pages/SwipeSessionPage.js
@@ -1,50 +1,186 @@
 import './SwipeSessionPage.css';
+import React, { useState, useRef, useMemo } from 'react';
 import Header from '../layouts/Header';
 import TinderCard from 'react-tinder-card';
-import Rating from '@mui/material/Rating';
-import StarIcon from '@mui/icons-material/Star';
+import { Rating } from 'react-simple-star-rating';
+
+const loaderData = [
+    {
+        name: '0',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '1',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '2',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    },
+    {
+        name: '3',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '4',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '5',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    },
+    {
+        name: '6',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '7',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '8',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    },
+    {
+        name: '9',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '10',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '11',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    },
+    {
+        name: '12',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '13',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '14',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    },
+    {
+        name: '15',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.4,
+        numberRatings: 1238
+    },
+    {
+        name: '16',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.7,
+        numberRatings: 92
+    },
+    {
+        name: '17',
+        imgUrl: '../images/RedRobin.jpg',
+        ratingValue: 4.8,
+        numberRatings: 1340
+    }
+]
 
 export default function SwipeSessionPage() {
+    const restaurants = loaderData;
+    const [currentIndex, setCurrentIndex] = useState(restaurants.length - 1);
+    const [lastDirection, setLastDirection] = useState();
+    const currentIndexRef = useRef(restaurants.length - 1);
 
-    const swiped = () => {
-
+    const updateCurrentIndex = (val) => {
+        setCurrentIndex(val)
+        currentIndexRef.current = val;
     }
+
+    const restaurantRefs = useMemo(
+        () => Array(restaurants.length).fill(0).map((i) => React.createRef()), []
+    )
+
+    const swiped = (direction, index) => {
+        setLastDirection(direction);
+        updateCurrentIndex(index - 1);
+      }
 
     return (
         <div>
             <Header />
             <div className="card-container">
-                <TinderCard
-                    preventSwipe={['up', 'down']}
-                    onCardLeftScreen={(dir) => {console.log(`Out of frame! Swipe direction: ${dir}`)}}
-                >
-                    <div className="card-box">
-                        <div className="card-image-container">
-                            <img className="card-image" src={require('../images/RedRobin.jpg')}/>
-                        </div>
-                        <div className="card-description-container">
-                            <div className="card-name-container">
-                                <h3 className="card-name">Red Robin Gourmet Burgers and Brews</h3>
-                                <div>
-                                    <h3 className="card-info-open-closed">Open</h3>
+                {restaurants.map((restaurant, index) => {
+                    return (
+                        <TinderCard
+                            ref={restaurantRefs[index]}
+                            key={index}
+                            className='card'
+                            preventSwipe={['up', 'down']}
+                            onSwipe={(dir) => swiped(dir, index)}
+                        >
+                            <div 
+                                className="card-box" 
+                                style={index == currentIndex || index === currentIndex - 1 || index === currentIndex + 1 ? {boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 10px'} : {boxShadow: 'none'}}
+                            >
+                                <div className="card-image-container">
+                                    <img className="card-image" src={require('../images/RedRobin.jpg')} />
+                                </div>
+                                <div className="card-description-container">
+                                    <div className="card-name-container">
+                                        <h3 className="card-name">{restaurant.name}</h3>
+                                        <div>
+                                            <h3 className="card-info-open-closed">Open</h3>
+                                        </div>
+                                    </div>
+                                    <div className="card-info">
+                                        <div className="card-info-rating">
+                                            <Rating 
+                                                initialValue={restaurant.ratingValue}
+                                                readonly={true}
+                                                allowFraction={true}
+                                                fillColor={'#FF9529'}
+                                                size={20}
+                                            />
+                                        </div>
+                                        <h3 className="card-info-stars">{restaurant.ratingValue} stars</h3>
+                                        <h4 className="card-info-number-ratings">{restaurant.numberRatings} ratings</h4>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="card-info">
-                                <div className="card-info-rating">
-                                    <Rating
-                                        readOnly
-                                        value={4.4}
-                                        precision={.1}
-                                        icon={<StarIcon style={{ color: '#FF9529' }} fontSize="inherit" />}
-                                        emptyIcon={<StarIcon style={{ color: '#484848' }} fontSize="inherit" />}
-                                    />
-                                </div>
-                                <h3 className="card-info-stars">4.4 stars</h3>
-                                <h4 className="card-info-number-ratings">1238 ratings</h4>
-                            </div>
-                        </div>
-                    </div>
-                </TinderCard>
+                        </TinderCard>
+                    )
+                }
+                )}
             </div>
         </div>
     )
