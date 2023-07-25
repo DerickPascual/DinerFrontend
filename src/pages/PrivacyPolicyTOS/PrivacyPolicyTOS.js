796b05da46b4b600c553d9ab73f68b7c7866da0d
Derick <derickvanpascual@gmail.com>
2023-07-25 01:21:09 -0700

    Changed Privacy policy and tos

--- src/pages/PrivacyPolicyTOS/PrivacyPolicyTOS.js
+++ src/pages/PrivacyPolicyTOS/PrivacyPolicyTOS.js
@@ -14,13 +14,13 @@ export default function PrivacyPolicyTOS() {
                     <p>Diner is a location-based app that allows users to swipe on restaurants using the Google Places API. We do not store any user information on our servers.</p>
                     <br></br>
                     <h3>2. Third Party Services</h3>
-                    <p>Diner uses the Google Places API for restaurant information. By using the app, you agree to be bound by <a href="https://policies.google.com/terms?hl=en" target="_blank">Google's Terms of Service</a> as well.</p>
+                    <p>Diner uses the Google Places API for restaurant information. By using the app, you agree to be bound by <a href="https://policies.google.com/terms?hl=en" target="_blank">Google's Terms of Service</a> 🤝 as well.</p>
                     <br></br>
                     <h3>3. Disclaimer of Liability</h3>
                     <p>Diner does not guarantee the accuracy, availability, or quality of the restaurant information provided. Users are advised to exercise their judgment when making dining decisions.</p>
                     <br></br>
                     <h3>4. Privacy</h3>
-                    <p>View our privacy policy below.</p>
+                    <p>We temporarily store user-inputted locations to query the Google Places API. After queries are completed, we do not retain this data. For more information, view our privacy policy below.</p>
                     <br></br>
                     <h3>5. Modification of Terms</h3>
                     <p>Diner reserves the right to update or modify these Terms of Service at any time. Continued use of the app after such changes will constitute your acceptance of the updated terms.</p>
@@ -37,10 +37,10 @@ export default function PrivacyPolicyTOS() {
                     <p>When you use our app, we may request access to your device's location services. Your location data will be used solely for the purpose of finding nearby restaurants and other related places using the Google Places API. We do not collect, store, or share your location data beyond the immediate query to the API.</p>
                     <br></br>
                     <h3>2. Use of Google Places API:</h3>
-                    <p>To provide you with information about nearby restaurants, our app uses the Google Places API. When you enter your location and start a swipe session, your device's location coordinates will be sent to Google's servers to retrieve relevant restaurant data. By reference, this means that by using our application, you are also bound by the <a href="https://policies.google.com/privacy" target="_blank">Google Privacy Policy</a>.</p>
+                    <p>To provide you with information about nearby restaurants, our app uses the Google Places API. When you enter your location and start a swipe session, your device's location coordinates will be sent to Google's servers to retrieve relevant restaurant data. By reference, this means that by using our application, you are also bound by the <a href="https://policies.google.com/privacy" target="_blank">Google Privacy Policy</a> 🔒.</p>
                     <br></br>
                     <h3>3. Data Storage</h3>
-                    <p>We do not store your location data or any personally identifiable information related to your location on our servers or in any other data storage facility.</p>
+                    <p>Diner temporarily stores user-inputted locations to perform a Google Places API query to find nearby restaurants. Once this query is completed, the data is not retained by our servers or stored in any external database or data storage facility. </p>
                     <br></br>
                     <h3>4. Information Sharing</h3>
                     <p>We do not share your location data with any third parties, except for transmitting it to Google's servers for the sole purpose of querying the Google Places API. We do not sell, rent, or lease your personal information to any other entity.</p>
@@ -49,7 +49,7 @@ export default function PrivacyPolicyTOS() {
                     <p>We use industry-standard encryption protocols to ensure data integrity during the API query process.</p>
                     <br></br>
                     <h3>6. Changes to Privacy Policy</h3>
-                    <p>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any significant changes by posting the updated policy on our website or within the app.</p>
+                    <p>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any significant changes by posting the updated policy on this website.</p>
                 </div>
             </div>
             <div></div>
